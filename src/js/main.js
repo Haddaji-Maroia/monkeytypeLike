4aa6bf5548a6a92814414878d1efa9a3f2c53179
Haddaji-Maroia <maroia.haddaji@student.hepl.be>
2025-07-14 13:38:51 +0200

    initial commit

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+import {paragraphs} from "./paragraphs";
+
+const pElement = document.getElementById('monkeytype');
+let currentWord = 0;
+let currentLetter = 0;
+
+
+
+//crazione elementi e parole 
